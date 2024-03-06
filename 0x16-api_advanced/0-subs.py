fecbc33960819cb150767dd3cc62c7fe97516081
Anana99 <ananaitohoinyene99@gmail.com>
2024-03-06 09:58:56 -0800

    0-subs.py1

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,21 +1,24 @@
 #!/usr/bin/python3
-'''this module contains the function number_of_subscribers
-'''
+"""
+Function that queries the Reddit API and returns the number of subscribers
+(not active users, total subscribers) for a given subreddit.
+If an invalid subreddit is given, the function should return 0
+"""
+
 import requests
 
-def top_ten(subreddit):
-    # Construct the API URL
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
 
-    # Make the API request
-    response = requests.get(url, headers={"User-Agent": "Reddit API Client"})
+def number_of_subscribers(subreddit):
+    """
+    Function that queries the Reddit API
+    - If not a valid subreddit, return 0.
+    """
+    req = requests.get(
+        "https://www.reddit.com/r/{}/about.json".format(subreddit),
+        headers={"User-Agent": "Custom"},
+    )
 
-    # Check if the subreddit exists
-    if response.status_code == 200:
-        data = response.json()
-        posts = data["data"]["children"]
-        for post in posts:
-            title = post["data"]["title"]
-            print(f"- {title}")
+    if req.status_code == 200:
+        return req.json().get("data").get("subscribers")
     else:
-        print("None")
+        return 0
