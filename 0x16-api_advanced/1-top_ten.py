788485915a3c44480af6bad47abbf29600827873
Anana99 <ananaitohoinyene99@gmail.com>
2024-03-06 09:46:23 -0800

    Update

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,21 +1,28 @@
 #!/usr/bin/python3
-"""Function to print hot posts on a given Reddit subreddit."""
-import requests
+""" 0x16. API advanced, task 1. Top Ten
+"""
+from requests import get
 
 
 def top_ten(subreddit):
-    """Print the titles of the 10 hottest posts on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
-    headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
-    }
-    params = {
-        "limit": 10
-    }
-    response = requests.get(url, headers=headers, params=params,
-                            allow_redirects=False)
-    if response.status_code == 404:
-        print("None")
+    """ Queries Reddit API and prints titles of first 10 "hot" posts listed for
+    a given subreddit.
+
+    Args:
+        subreddit (str): subreddit to query
+    """
+    # adding request parameter `raw_json` deactivates default ampersand escape
+    url = 'https://www.reddit.com/r/{}/hot.json?raw_json=1'
+    response = get(url.format(subreddit),
+                   headers={'User-Agent': 'allelomorph-app1'})
+    if response.status_code != 200:
+        print(None)
         return
-    results = response.json().get("data")
-    [print(c.get("data").get("title")) for c in results.get("children")]
+    hot_posts = response.json().get('data').get('children', [])
+    if not hot_posts:
+        print(None)
+        return
+    for i, post in enumerate(hot_posts):
+        if i == 10:
+            break
+        print(post.get('data').get('title', ''))
--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,30 +1,30 @@
 #!/usr/bin/python3
-"""Function to query a list of all hot posts on a given Reddit subreddit."""
-import requests
+""" 0x16. API advanced, task 2. Recurse it!
+"""
+from requests import get
 
 
-def recurse(subreddit, hot_list=[], after="", count=0):
-    """Returns a list of titles of all hot posts on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
-    headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
-    }
-    params = {
-        "after": after,
-        "count": count,
-        "limit": 100
-    }
-    response = requests.get(url, headers=headers, params=params,
-                            allow_redirects=False)
-    if response.status_code == 404:
+def recurse(subreddit, hot_list=[]):
+    """ Recursively queries Reddit API, one page per frame, to compile list of
+    all "hot" posts for a given subreddit.
+
+    Args:
+        subreddit (str): subreddit to query
+        hot_list (list): list of dictionaries representing posts in "hot",
+            compiled from lower frames in the stack
+
+    Return:
+        `hot_list` with the current API page/recursion frame's list appended
+    """
+    limit = 100
+    after = hot_list[-1].get('data').get('name', '') if len(hot_list) else ''
+    # adding request parameter `raw_json` deactivates default ampersand escape
+    url = 'https://www.reddit.com/r/{}/hot.json?raw_json=1&after={}&limit={}'
+    response = get(url.format(subreddit, after, limit),
+                   headers={'User-Agent': 'allelomorph-app2'})
+    if response.status_code != 200:
         return None
-
-    results = response.json().get("data")
-    after = results.get("after")
-    count += results.get("dist")
-    for c in results.get("children"):
-        hot_list.append(c.get("data").get("title"))
-
-    if after is not None:
-        return recurse(subreddit, hot_list, after, count)
-    return hot_list
+    current_page_list = response.json().get('data').get('children', [])
+    if len(current_page_list) < limit:
+        return hot_list + current_page_list
+    return recurse(subreddit, hot_list + current_page_list)
